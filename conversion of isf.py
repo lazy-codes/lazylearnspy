6ae9bfa1a16197b84d3bf2852d2fc3750a5e9125
lazy-codes <demonicscalan@gmail.com>
2026-06-30 17:00:58 +0530

    Add files via upload

---
+++ conversion of isf.py
@@ -0,0 +1,24 @@
+#conversion of integer float and string
+
+f = 2.00
+i = 2
+s = "2"
+
+f_to_i = int(f)
+i_to_s = str(i)
+s_to_f = float(s)
+
+print("f_to_i:", f, ">", f_to_i)
+print("i_to_s:", i, ">", i_to_s, type(i_to_s))
+print("s_to_f:", s, ">", s_to_f), type(s_to_f)
+
+
+
+#celcius to fahrenheit
+
+celcius = int(input("Enter temp in celcius:"))
+
+fahrenheit = (celcius * 9/5) + 32
+
+print(f" {celcius} -> {fahrenheit}\n Here is your conversion.")
+
